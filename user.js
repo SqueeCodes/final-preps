8c9fb34a6f4374e735ccd491478665413bf5ecc6
David <itsmesquee@gmail.com>
2024-06-29 11:36:35 -0600

    finished api fetches

--- user.js
+++ user.js
@@ -1,10 +1,19 @@
 const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem("id")
+
 
 async function onSearchChange(event) {
   const id = event.target.value;
+  renderPosts(id)
+}
+
+async function renderPosts(userId) {
   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
   const postsData = await posts.json();
-  postListEl.innerHTML = postsData.map(post => 
+  postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
+}
+
+function postHTML(post) {
     `<div class="post">
       <div class="post__title">
         ${post.title}
@@ -13,26 +22,6 @@ async function onSearchChange(event) {
         ${post.body}
       </p>
     </div>`
-  ).join('');
 }
 
-
-async function main() {
-  const id = localStorage.getItem("id")
-  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-  const postsData = await posts.json();
-  console.log(postsData)
-
-  postListEl.innerHTML = postsData.map(post => 
-    `<div class="post">
-      <div class="post__title">
-        ${post.title}
-      </div>
-      <p class="post__body">
-        ${post.body}
-      </p>
-    </div>`
-  ).join('');
-}
-
-main();
+renderPosts(id);
